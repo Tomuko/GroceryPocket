4f36781182d7ee9e819cb27e790c0acd6b042f77
Tomuko <vika_stradzina@mail.ru>
2023-08-05 15:24:54 +0300

    Initial Commit

---
+++ GroceryPocket/GroceryPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryPocketApp.swift
+//  GroceryPocket
+//
+//  Created by Виктория Страдзина on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GroceryPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
